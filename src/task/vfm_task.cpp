8cb6094257b364db8b1eb27864eff98deb9877be
Andres Utrera <a.i.utrera@rug.nl>
2025-10-26 12:37:28 +0100

    adding logfile

--- src/task/vfm_task.cpp
+++ src/task/vfm_task.cpp
@@ -1,10 +1,55 @@
 #include "vfm_task.h"
 #include <FECore/log.h>
+#include <FEBioLib/FEBioModel.h>
 #include "optimization/vfm_solver.hpp"
 #include "io/exporter.hpp"
 #include "diag/felog_bridge.hpp"
 #include "diag/printers/param_table.hpp"
 #include <filesystem>
+
+namespace
+{
+// Configure FEBio logging so feLog also writes to disk.
+enum class LogSetupResult
+{
+    NotApplicable,
+    Enabled,
+    Failed
+};
+
+LogSetupResult ensure_vfm_logfile(FEModel *fem,
+                                  const std::string &inputPath,
+                                  std::string &outPath)
+{
+    auto *febio = dynamic_cast<FEBioModel *>(fem);
+    if (febio == nullptr)
+        return LogSetupResult::NotApplicable;
+
+    std::filesystem::path logPath = inputPath.empty() ?
+                                        std::filesystem::path("vfm.log") :
+                                        std::filesystem::path(inputPath).replace_extension(".log");
+
+    outPath = logPath.string();
+
+    Logfile &logFile = febio->GetLogFile();
+    if (logFile.is_valid())
+    {
+        if (logFile.FileName() == outPath)
+        {
+            logFile.SetMode(Logfile::LOG_FILE_AND_SCREEN);
+            return LogSetupResult::Enabled;
+        }
+        logFile.close();
+    }
+
+    febio->SetLogFilename(outPath);
+    if (!logFile.open(outPath.c_str()))
+        return LogSetupResult::Failed;
+
+    logFile.SetMode(Logfile::LOG_FILE_AND_SCREEN);
+    return LogSetupResult::Enabled;
+}
+} // namespace
 
 VFMTask::VFMTask(FEModel *fem) : FECoreTask(fem) {}
 
@@ -12,6 +57,8 @@ bool VFMTask::Init(const char *xmlPath)
 {
     m_inputPath = (xmlPath && *xmlPath) ? xmlPath : std::string{};
     diag::ScopedFEBind bind(GetFEModel());
+    std::string logFilePath;
+    const LogSetupResult logSetup = ensure_vfm_logfile(GetFEModel(), m_inputPath, logFilePath);
     feLog("\n");
     feLog("===========================================================================\n");
     feLog("                        VIRTUAL FIELDS METHOD (VFM)                        \n");
@@ -21,6 +68,14 @@ bool VFMTask::Init(const char *xmlPath)
     feLog("                                   SETUP                                   \n");
     feLog("...........................................................................\n");
     feLog("\n\n");
+    if (logSetup == LogSetupResult::Enabled && !logFilePath.empty())
+    {
+        feLog("Log file: %s\n\n", logFilePath.c_str());
+    }
+    else if (logSetup == LogSetupResult::Failed && !logFilePath.empty())
+    {
+        feLogWarning("Failed to open VFM log file at %s\n\n", logFilePath.c_str());
+    }
 
     std::string err;
     VFMXmlReader rdr;
