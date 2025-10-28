d7909a51c36698d220ead34f74fefb7f30a05893
Andres Utrera <a.i.utrera@rug.nl>
2025-10-28 10:10:30 +0100

    update plugin version

--- src/plugin/register.cpp
+++ src/plugin/register.cpp
@@ -1,6 +1,5 @@
 #include <FECore/FECoreKernel.h>
-
-// #include "VFM.h"
+#include "version.h"
 #include "task/vfm_task.h"
 
 FECORE_EXPORT unsigned int GetSDKVersion()
@@ -8,13 +7,15 @@ FECORE_EXPORT unsigned int GetSDKVersion()
     return FE_SDK_VERSION;
 }
 
+FECORE_EXPORT void GetPluginVersion(int &major, int &minor, int &patch)
+{
+    major = VERSION;
+    minor = SUBVERSION;
+    patch = SUBSUBVERSION;
+}
+
 FECORE_EXPORT void PluginInitialize(FECoreKernel &febio)
 {
     FECoreKernel::SetInstance(&febio);
     REGISTER_FECORE_CLASS(VFMTask, "VFM");
-}
-
-FECORE_EXPORT void PluginCleanup()
-{
-    // No resources to release yet.
 }
---
+++ src/plugin/version.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VERSION 1
+#define SUBVERSION 0
+#define SUBSUBVERSION 0
